393b460e95a4f6c7643bf101e1fa1295bef589e9
Frenz86 <frenzy86@gmail.com>
2020-11-04 14:45:11 +0100

    ww

--- pag2.py
+++ pag2.py
@@ -41,7 +41,7 @@ def main():
     # Get the data from url and request it as json file
     st.button("Re-run")
     st.title("Advertising Analytics")
-    image = Image.open('./image/silvi.jpg')
+    image = Image.open('./image/Silvi1.jpeg')
     st.image(image,use_column_width=True)
 
     #% VENDITE LINEA1
@@ -135,9 +135,11 @@ def main():
 
     st.markdown("** CANALI DI MARKETING: **")
     st.markdown("- TV")
-    st.markdown("- SEO")
-    st.markdown("- ADWORDS")
-    st.markdown("- PAPER")
+    st.markdown("- E-MAIL MARKETING")
+    st.markdown("- FACEBOOK-GOOGLE")
+    st.markdown("- VOUCHER CARTACEI")
+    st.markdown("- EVENTI")
+
     
     #% LINEAR OPTIMIZATION ADVERTISING BUDGET
     chan1=st.number_input('% BUDGET ON TV',value=15,max_value=100,step=1) 
@@ -146,50 +148,60 @@ def main():
                           min_value=0.00,
                           value=0.12,
                           step=0.01)
-    chan2=st.number_input('% BUDGET ON SEO',value=100-chan1,max_value=100,step=1)
-    chan2_rate = st.slider(label="Impostare la conversion rate SEO",
+    chan2=st.number_input('% BUDGET E-MAIL MARKETING',value=100-chan1,max_value=100,step=1)
+    chan2_rate = st.slider(label="Impostare la conversion rate E-MAIL MARKETING",
                           max_value=1.00,
                           min_value=0.00,
                           value=0.09,
                           step=0.01)
-    chan3=st.number_input('% BUDGET ON ADWORDS',value=100-chan1-chan2,max_value=100,step=1)
-    chan3_rate = st.slider(label="Impostare la conversion rate ADWORDS",
+    chan3=st.number_input('% BUDGET FACEBOOK-GOOGLE',value=100-chan1-chan2,max_value=100,step=1)
+    chan3_rate = st.slider(label="Impostare la conversion rate FACEBOOK-GOOGLE",
                           max_value=1.00,
                           min_value=0.00,
                           value=0.12,
                           step=0.01)
-    chan4=st.number_input('% BUDGET ON PAPER',value=100-chan1-chan2-chan3,max_value=100,step=1)
-    chan4_rate = st.slider(label="Impostare la conversion rate PAPER",
+    chan4=st.number_input('% BUDGET VOUCHER CARTACEI',value=100-chan1-chan2-chan3,max_value=100,step=1)
+    chan4_rate = st.slider(label="Impostare la conversion rate VOUCHER CARTACEI",
                           max_value=1.00,
                           min_value=0.00,
                           value=0.11,
+                          step=0.01)
+    chan5=st.number_input('% BUDGET EVENTI ',value=100-chan1-chan2-chan3-chan4,max_value=100,step=1)
+    chan5_rate = st.slider(label="Impostare la conversion rate EVENTI",
+                          max_value=1.00,
+                          min_value=0.00,
+                          value=0.06,
                           step=0.01)
     ## ON TOTAL
     unit_sold_SEO = int(ad_budget*chan1/100*chan1_rate)
     unit_sold_TV  = int(ad_budget*chan2/100*chan2_rate)
     unit_sold_ADWORDS = int(ad_budget/100*chan3*chan3_rate)
     unit_sold_PAPER = int(ad_budget/100*chan4*chan4_rate)
+    unit_sold_EVENT = int(ad_budget/100*chan5*chan5_rate)
     
     ## ON PROD1
     unit_sold_SEO1 = unit_sold_SEO*var1/100
     unit_sold_TV1  = unit_sold_TV*var1/100
     unit_sold_ADWORDS1 = unit_sold_ADWORDS*var1/100
     unit_sold_PAPER1 = unit_sold_PAPER*var1/100
-    unit_sold1 = int(unit_sold_SEO1+unit_sold_TV1+unit_sold_ADWORDS1+unit_sold_PAPER1)
+    unit_sold_EVENT1 = unit_sold_EVENT*var1/100
+    unit_sold1 = int(unit_sold_SEO1+unit_sold_TV1+unit_sold_ADWORDS1+unit_sold_PAPER1+unit_sold_EVENT1)
     
     ## ON PROD2
     unit_sold_SEO2 = unit_sold_SEO*var2/100
     unit_sold_TV2  = unit_sold_TV*var2/100
     unit_sold_ADWORDS2 = unit_sold_ADWORDS*var2/100
     unit_sold_PAPER2 = unit_sold_PAPER*var2/100
-    unit_sold2 = int(unit_sold_SEO2+unit_sold_TV2+unit_sold_ADWORDS2+unit_sold_PAPER2)
+    unit_sold_EVENT2 = unit_sold_EVENT*var2/100
+    unit_sold2 = int(unit_sold_SEO2+unit_sold_TV2+unit_sold_ADWORDS2+unit_sold_PAPER2+unit_sold_EVENT2)
     
-    ## ON PROD2
+    ## ON PROD3
     unit_sold_SEO3 = unit_sold_SEO*var3/100
     unit_sold_TV3  = unit_sold_TV*var3/100
     unit_sold_ADWORDS3 = unit_sold_ADWORDS*var3/100
     unit_sold_PAPER3 = unit_sold_PAPER*var3/100
-    unit_sold3 = int(unit_sold_SEO3+unit_sold_TV3+unit_sold_ADWORDS3+unit_sold_PAPER3)  
+    unit_sold_EVENT3 = unit_sold_EVENT*var3/100
+    unit_sold3 = int(unit_sold_SEO3+unit_sold_TV3+unit_sold_ADWORDS3+unit_sold_PAPER3+unit_sold_EVENT3)  
     
     
     # unit_sold = unit_sold1+unit_sold2+unit_sold3
@@ -202,40 +214,56 @@ def main():
     ## Image 2
     st.markdown("**Costi dovuti all'aumento della produzione**")
 
-    image = Image.open('./image/Silvi1.jpeg')
-    st.image(image,use_column_width=True)
-
     sales_expense = st.slider(label="Impostare costi extra dovuti alle vendite",
                         max_value=20000,
                         min_value=1000,
                         value=2500,
                         step=100)
     
-    marketing_expense = st.slider(label="Costi gestione campagne marketing (h attività, etc)",
+    marketing_expense = st.slider(label="Costi gestione campagne marketing (h attività, etc) ed altri costi",
                         max_value=20000,
                         min_value=1000,
                         value=1500,
                         step=100)
 
+    CTOT = ad_budget+sales_expense+marketing_expense
     if var1!=0 & var2==0 & var3==0:
-        profit_item = unit_sold1*(sum(price_item1)/len(price_item1))-sales_expense-marketing_expense
+        profit_item = unit_sold1*(sum(price_item1)/len(price_item1))
     elif var3!=0 & var2!=0:
-        profit_item = unit_sold1*(sum(price_item1)/len(price_item1))+unit_sold2*(sum(price_item2)/len(price_item2))-sales_expense-marketing_expense
+        profit_item = unit_sold1*(sum(price_item1)/len(price_item1))+unit_sold2*(sum(price_item2)/len(price_item2))
     
-    #profit_itemz = int(sum(profit_item1) / len(profit_item1))+int(sum(profit_item2)/ len(profit_item2))-sales_expense-marketing_expense
+    #Competition reaction:
+    st.markdown("**Selezionare la Competition reaction: **")
+    options = {1.0: "Nessuna",
+            0.98: "Bassa",
+            0.96: "Media",
+            0.92: "Alta",
+        }
+
+    comp_react = st.selectbox("", list(options.items()), 0 , format_func=lambda o: o[1])
+    #print(comp_react[0])
+    profit_item = profit_item*(comp_react[0])
     print(profit_item)
-    ROI = (profit_item-ad_budget)/ad_budget*100
+    utile_campagne = profit_item-CTOT
+    utile_campagne = np.random.triangular(size=N, left=utile_campagne-100, right=utile_campagne+100, mode=utile_campagne)
+    print(utile_campagne)
+
+    ROI = (profit_item-CTOT)/CTOT*100
     print(ROI)
+ 
 
     #Result
-    if st.checkbox(' Mostra i Risultati'):
+    if st.checkbox('Mostra i Risultati'):
         #st.markdown(f"Probabilità di break-even (ovvero per generare profitto positivo): **{prob_profit*100: .2f}%**")
+        st.markdown(f"Il costo totale medio delle campagne: **€{int(CTOT)}**")
+        st.markdown(f"Operative Income medio generato delle campagne: **€{int(sum(utile_campagne) / len(utile_campagne))}**")
         st.markdown(f"La redditività media dell'investimento (ROI) del Marketing Budget: "
-                f"**{(profit_item- ad_budget) / (ad_budget)*100: .2f}%**")
-                #f"**{(sum(profit_item) / len(profit_item) - ad_budget) *100 / (ad_budget): .2f}%**")
-        #st.markdown(f"Utile medio: **€{sum(profit_item1) / len(profit_item1): .2f}**")
+                f"**{(profit_item- CTOT) / (CTOT)*100: .2f}%**")
         #st.markdown(f"ROI: **{((sum(profit_item1) / len(profit_item1)+sum(profit_item2)/ len(profit_item2))- ad_budget-sales_expense-marketing_expense) *100 / (ad_budget): .2f}**")
-
+        hist_data = [utile_campagne]
+        group_label = ['Utile campagne marketing']
+        fig = ff.create_distplot(hist_data, group_label, bin_size=[3], curve_type='normal')
+        st.plotly_chart(fig, use_container_width=True)  
 
 if __name__ == "__main__":
     main()
